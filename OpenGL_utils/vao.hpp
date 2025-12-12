fad978149fdff33e8eba4a7111448d8d8192c8df
NoosCrim <tomasz.zuranski87@gmail.com>
2025-12-12 21:21:32 +0100

    Added VAO wrapper class
    Created utility RAII VAO wrapper. Opposite to buffer wrappers, it is not reference counted and cannot be coppied

---
+++ OpenGL_utils/vao.hpp
@@ -0,0 +1,82 @@
+#include <GL/glew.h>
+namespace render
+{
+    class VAO
+    {
+    private:
+        GLuint _name;
+    protected:
+        GLuint activeAttribBitfield = 0u;
+    public:
+        VAO()
+        {
+            glGenVertexArrays(1, &_name);
+        }
+        ~VAO()
+        {
+            glDeleteVertexArrays(1, &_name);
+        }
+        VAO(const VAO&) = delete;
+        VAO& operator=(const VAO&) = delete;
+        VAO(VAO&& other) noexcept : _name(other._name), activeAttribBitfield(other.activeAttribBitfield)
+        {
+            other._name = 0;
+            other.activeAttribBitfield = 0u;
+        }
+        VAO& operator=(VAO&& other) noexcept
+        {
+            if (this != &other)
+            {
+                if(_name) glDeleteVertexArrays(1, &_name);
+                _name = other._name;
+                activeAttribBitfield = other.activeAttribBitfield;
+                other._name = 0;
+                other.activeAttribBitfield = 0u;
+            }
+            return *this;
+        }
+        inline GLuint name() const
+        {
+            return _name;
+        }
+        inline operator GLuint() const
+        {
+            return _name;
+        }
+        inline operator bool() const
+        {
+            return _name;
+        }
+        inline bool IsAttribActive(GLuint attribIndex) const
+        {
+            return (activeAttribBitfield & (1u << attribIndex)) != 0u;
+        }
+        inline void EnableAttrib(GLuint attribIndex)
+        {
+            activeAttribBitfield |= (1u << attribIndex);
+            glEnableVertexArrayAttrib(_name, attribIndex);
+        }
+        inline void DisableAttrib(GLuint attribIndex)
+        {
+            activeAttribBitfield &= ~(1u << attribIndex);
+            glDisableVertexArrayAttrib(_name, attribIndex);
+        }
+        inline void BindVertexBuffer(GLuint bindingIndex, GLuint bufferName, GLintptr offset, GLsizei stride)
+        {
+            glVertexArrayVertexBuffer(_name, bindingIndex, bufferName, offset, stride);
+        }
+        inline void BindElementBuffer(GLuint bufferName)
+        {
+            glVertexArrayElementBuffer(_name, bufferName);
+        }
+        inline void Draw(GLuint first, GLuint count, GLenum mode = GL_TRIANGLES)
+        {
+            glDrawArrays(GL_TRIANGLES, first, count);
+        }
+        inline void DrawInstanced(GLuint first, GLuint count, GLuint instanceCount, GLenum mode = GL_TRIANGLES)
+        {
+            glDrawArraysInstanced(mode, first, count, instanceCount);
+        }
+
+    };
+} // namespace name
